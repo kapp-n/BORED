644611c4483d226651f02dc453f39039fa957d76
Mollie Dickenson <molliedickenson@gmail.com>
2021-04-09 12:26:14 -0400

    finishing up

--- src/index.js
+++ src/index.js
@@ -24,10 +24,45 @@ function randomize(){
 function getActivities(){
     let buttons = document.getElementById('buttons')
     buttons.innerHTML = ''
-    buttons.innerHTML += '<select> <option value ="recreational">Recreational Activity</option><option value="education">Educational Acitvity</option><option value="relax">Relaxational Acitivity</option></select>'
-    document.getElementsByClassName('button').addEventListener('click', randomActivity)
+    buttons.innerHTML += '<form name="select" id="form"> <select id="select"> <option value ="recreational">Recreational Activity</option><option value="education">Educational Acitvity</option><option value="relax">Relaxational Acitivity</option></select><input type="submit" value="Go"></form>'
+    document.querySelector('#form').addEventListener('submit', getActivity)
 }
 
-// function randomActivity(){
-//     switch(true)
-// }
+function getActivity(event){
+    event.preventDefault()
+    console.log(event)
+    let theActivity = document.getElementById('select').value
+    let activities = document.getElementById('activity')
+    if (theActivity === 'recreational'){
+        fetch(BASE_URL + '/activity?type=recreational')
+        .then(res => res.json())
+        .then(activity => {
+            console.log(activity)
+            activities.innerHTML = `
+            <li>Activity: ${activity.activity}</li>
+            <li> Type: ${activity.type}</li>
+            <li> Particpants: ${activity.particpants}</li>`
+        })
+    } else if (theActivity ==='education'){
+        fetch(BASE_URL + '/activity?type=education')
+        .then(res => res.json())
+        .then(activity => {
+            console.log(activity)
+            activities.innerHTML = `
+            <li>Activity: ${activity.activity}</li>
+            <li> Type: ${activity.type}</li>
+            <li> Particpants: ${activity.particpants}</li>`
+        })
+    } else {
+        fetch(BASE_URL + '/activity?type=relaxation')
+        .then(res => res.json())
+        .then(activity => {
+            console.log(activity)
+            activities.innerHTML = `
+            <li>Activity: ${activity.activity}</li>
+            <li> Type: ${activity.type}</li>
+            <li> Particpants: ${activity.particpants}</li>`
+        })
+    }
+}
+
